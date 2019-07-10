d69f6c55e96a6f6c7f18e57fb5aa0ec9533353e4
MahmoudBwedany <Mahmoudbwedany@gmail.com>
2019-07-10 09:12:42 +0300

    created the variables

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = "Mahmoud" ;
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + " !";
+
+let excitedWelcomeMessage =  "WELCOME TO FLATBOOK, " + currentUser + " !";
+
+let shortGreeting = "Welcome, " + currentUser[0] + " !";
