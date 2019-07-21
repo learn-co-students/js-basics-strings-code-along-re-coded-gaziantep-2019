cccca5a15b5482d65d44d71439cd02a888e25295
Amjad Arab <amjad.arab99@gmail.com>
2019-07-21 10:36:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+let currentUser = "string";
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+let shortGreeting = "Welcome, " + firstInitial + "!";
